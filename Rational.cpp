40e869647a8f1ac0c7fa35f373c5bf6dccd0d30f
ChocolateCharlie <aurore.amrit@gmail.com>
2021-08-10 15:45:07 +0200

    Create Rational class

---
+++ Rational.cpp
@@ -0,0 +1,6 @@
+#include "Rational.h"
+
+// CONSTRUCTOR
+Rational::Rational(int numerator, int denominator) : m_numerator(numerator), m_denominator(denominator) {
+    if (m_denominator == 0) { m_denominator = 1; }  // Arbitrarily changes to a valid denominator
+}
---
+++ Rational.h
@@ -0,0 +1,16 @@
+#ifndef RATIONAL_H_INCLUDED
+#define RATIONAL_H_INCLUDED
+
+class Rational {
+    public:
+
+    Rational(int numerator, int denominator = 1);
+
+    private:
+
+    int m_numerator;
+    int m_denominator;
+
+};
+
+#endif // RATIONAL_H_INCLUDED
