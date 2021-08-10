be875e428d98c189b9382cce29ca840f0c19ea13
ChocolateCharlie <aurore.amrit@gmail.com>
2021-08-10 17:25:20 +0200

    Fix basic-functions header

--- basic-functions.h
+++ basic-functions.h
@@ -1,8 +1,8 @@
-#ifndef BASIC-FUNCTIONS_H_INCLUDED
-#define BASIC-FUNCTIONS_H_INCLUDED
+#ifndef BASIC_FUNCTIONS_H_INCLUDED
+#define BASIC_FUNCTIONS_H_INCLUDED
 
 int gcd(int a, int b);
 int max(const int &a, const int &b);    // WARNING: same name as in <algorithm>
 int min(const int &a, const int &b);    // WARNING: same name as in <algorithm>
 
-#endif // BASIC-FUNCTIONS_H_INCLUDED
+#endif // BASIC_FUNCTIONS_H_INCLUDED
