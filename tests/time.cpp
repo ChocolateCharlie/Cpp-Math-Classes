85d5400202e912d60252eed0675c0fca936e86e7
ChocolateCharlie <aurore.amrit@gmail.com>
2021-11-11 14:17:37 +0100

    Sets up for Time class testing
    Creates src and tests folders

---
+++ tests/time.cpp
@@ -0,0 +1,10 @@
+/* ************************************************************************* */
+/* time.cpp                                                            TESTS */
+/* By: ChocolateCharlie                                                      */
+/* Updated: 2021-11-11                                                       */
+/* ************************************************************************* */
+#define BOOST_TEST_MODULE Time
+#include <boost/test/included/unit_test.hpp>
+#include "../src/Time.h"
+#include "../src/Time.cpp"
+
