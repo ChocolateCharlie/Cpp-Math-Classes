011e90b6a7042fa3d489b78a615436de8194d087
ChocolateCharlie <aurore.amrit@gmail.com>
2021-11-11 14:00:48 +0100

    Sets up for Time class testing
    Comments out most of Time class

--- Time.cpp
+++ Time.cpp
@@ -1,14 +1,22 @@
+/* ************************************************************************* */
+/* Time.cpp                                                         CPP-MATH */
+/* By: ChocolateCharlie                                                      */
+/* Updated: 2021-11-11                                                       */
+/* ************************************************************************* */
 #include <iomanip>
 #include <iostream>
 
 #include "Time.h"
 
 // CONSTRUCTOR
+/*
 Time::Time(int hours, int minutes, int seconds) : m_hours(hours), m_minutes(minutes), m_seconds(seconds) {
-    make_valid();
+	make_valid();
 }
+*/
 
 // OPERATOR METHODS
+/*
 Time& Time::operator+=(const Time &t) {
     m_seconds += t.m_seconds;
     m_minutes += t.m_minutes;
@@ -16,7 +24,7 @@ Time& Time::operator+=(const Time &t) {
     make_valid();
     return(*this);
 }
-
+*//*
 Time& Time::operator-=(const Time &t) {
     m_seconds -= t.m_seconds;
     m_minutes -= t.m_minutes;
@@ -24,7 +32,7 @@ Time& Time::operator-=(const Time &t) {
     make_valid();
     return(*this);
 }
-
+*//*
 Time& Time::operator*=(const int &n) {
     m_seconds *= n;
     m_minutes *= n;
@@ -32,7 +40,7 @@ Time& Time::operator*=(const int &n) {
     make_valid();
     return(*this);
 }
-
+*//*
 Time& Time::operator/=(const int &n) {
     m_seconds /= n;
     m_minutes /= n;
@@ -40,7 +48,7 @@ Time& Time::operator/=(const int &n) {
     make_valid();
     return(*this);
 }
-
+*//*
 Time& Time::operator%=(const int &n) {
     m_seconds %= n;
     m_minutes %= n;
@@ -48,24 +56,26 @@ Time& Time::operator%=(const int &n) {
     make_valid();
     return(*this);
 }
-
+*/
 // PRIVATE METHODS
 // PRIVATE CONSTANT METHODS
+/*
 bool Time::is_equal_to(const Time &t) const {
     return(m_hours == t.m_hours && m_minutes == t.m_minutes && m_seconds == t.m_seconds);
 }
-
+*//*
 bool Time::is_smaller_than(const Time &t) const {
     return(m_hours < t.m_hours || (m_hours == t.m_hours && (m_minutes < t.m_minutes || (m_minutes == t.m_minutes && m_seconds < t.m_seconds))));
 }
-
+*//*
 void Time::show(std::ostream &stream) const {
     stream << std::setfill('0') << std::setw(2) << m_hours << ":";
     stream << std::setfill('0') << std::setw(2) << m_minutes << ":";
     stream << std::setfill('0') << std::setw(2) << m_seconds;
 }
-
+*/
 // OTHER PRIVATE METHODS
+/*
 void Time::make_valid() {
     if (m_seconds < 0)  { m_seconds = 0; }
     if (m_minutes < 0)  { m_minutes = 0; }
@@ -81,48 +91,51 @@ void Time::make_valid() {
         m_minutes %= 60;
     }
 }
-
+*/
 /* ---------- OUT OF CLASS ---------- */
 
 // FRIEND FUNCTIONS
+/*
 bool operator==(const Time &a, const Time &b) {
     return(a.is_equal_to(b));
 }
-
+*//*
 bool operator!=(const Time &a, const Time &b) {
     return(!a.is_equal_to(b));
 }
-
+*//*
 bool operator<(const Time &a, const Time &b) {
     return(a.is_smaller_than(b));
 }
-
+*//*
 bool operator<=(const Time &a, const Time &b) {
     return(!b.is_smaller_than(a));
 }
-
+*//*
 bool operator>(const Time &a, const Time &b) {
     return(b.is_smaller_than(a));
 }
-
+*//*
 bool operator>=(const Time &a, const Time &b) {
     return(!a.is_smaller_than(b));
 }
-
+*//*
 std::ostream& operator<<(std::ostream &stream, const Time &t) {
     t.show(stream);
     return(stream);
 }
-
+*/
 // OPERATOR FUNCTIONS
+/*
 Time operator+(const Time &a, const Time &b) {
     Time temp(a);
     temp += b;
     return(temp);
 }
-
+*//*
 Time operator-(const Time &a, const Time &b) {
     Time temp(a);
     temp -= b;
     return(temp);
 }
+*/
--- Time.h
+++ Time.h
@@ -1,39 +1,46 @@
+/* ************************************************************************* */
+/* Time.h                                                           CPP-MATH */
+/* By: ChocolateCharlie                                                      */
+/* Updated: 2021-11-11                                                       */
+/* ************************************************************************* */
 #ifndef TIME_H_INCLUDED
 #define TIME_H_INCLUDED
 
 #include <iostream>
 
 class Time {
-    public:
-
-    Time(int hours = 0, int minutes = 0, int seconds = 0);
-    Time& operator+=(const Time &t);
-    Time& operator-=(const Time &t);
-    Time& operator*=(const int &n); // Multiplies hours, minutes and seconds by n
-    Time& operator/=(const int &n); // Divides hours, minutes and seconds by n
-    Time& operator%=(const int &n); // Moduloes hours, minutes and seconds by n
-
-    private:
-
-    bool is_equal_to(const Time &t) const;
-    bool is_smaller_than(const Time &t) const;
-    void make_valid();
-    void show(std::ostream &stream = std::cout) const;
-
-    int m_hours;
-    int m_minutes;
-    int m_seconds;
-
-    friend bool operator==(const Time &a, const Time &b);
-    friend bool operator!=(const Time &a, const Time &b);
-    friend bool operator< (const Time &a, const Time &b);
-    friend bool operator<=(const Time &a, const Time &b);
-    friend bool operator> (const Time &a, const Time &b);
-    friend bool operator>=(const Time &a, const Time &b);
-    friend std::ostream& operator<<(std::ostream &stream, const Time &t);
+	public:
+		/*
+		Time(int hours = 0, int minutes = 0, int seconds = 0);
+		Time& operator+=(const Time &t);
+		Time& operator-=(const Time &t);
+		Time& operator*=(const int &n);	// Multiplies hours, minutes and seconds by n
+		Time& operator/=(const int &n);	// Divides hours, minutes and seconds by n
+		Time& operator%=(const int &n);	// Moduloes hours, minutes and seconds by n
+		*/
+	private:
+		/*
+		bool is_equal_to(const Time &t) const;
+		bool is_smaller_than(const Time &t) const;
+		void make_valid();
+		void show(std::ostream &stream = std::cout) const;
+		*//*
+		int m_hours;
+		int m_minutes;
+		int m_seconds;
+		*//*
+		friend bool operator==(const Time &a, const Time &b);
+		friend bool operator!=(const Time &a, const Time &b);
+		friend bool operator< (const Time &a, const Time &b);
+		friend bool operator<=(const Time &a, const Time &b);
+		friend bool operator> (const Time &a, const Time &b);
+		friend bool operator>=(const Time &a, const Time &b);
+		friend std::ostream& operator<<(std::ostream &stream, const Time &t);
+		*/
 };
 
 Time operator+(const Time &a, const Time &b);
 Time operator-(const Time &a, const Time &b);
 
-#endif // TIME_H_INCLUDED
+#endif	// TIME_H_INCLUDED
+
